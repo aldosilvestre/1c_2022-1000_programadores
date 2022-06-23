7654684619303e904bc820be5a9a65de527fb73f
Aldo Silvestre <aldosilvestre89@gmail.com>
2022-06-22 22:24:51 -0300

    clase 28 CN1 CN7 fix

--- CN1/Clase28/query.sql
+++ CN1/Clase28/query.sql
@@ -5,10 +5,10 @@ create database clase28;
 use clase28;
 
 create table USUARIO(
-	id INT(11) primary key auto_increment,
-	username VARCHAR(255) unique not null,
+  id INT(11) primary key auto_increment,
+  username VARCHAR(255) unique not null,
   password VARCHAR(255) not null,
-	nombre VARCHAR(50) not null,
+  nombre VARCHAR(50) not null,
   apellido VARCHAR (50) not null,
   email VARCHAR(255) not null,
   is_admin BIT default 0
